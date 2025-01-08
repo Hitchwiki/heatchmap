3a9afd946b8842368ccbdba32d4c0b6c02e8c129
Till Wenke <38500891+tillwenke@users.noreply.github.com>
2025-01-08 23:06:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="heatchmap",
-    version="0.1.0",
+    version="0.3.0",
     author="Hitchwiki",
     author_email="info@hitchwiki.org",
     description="A package for estimation and visualization of hitchhiking quality.",
